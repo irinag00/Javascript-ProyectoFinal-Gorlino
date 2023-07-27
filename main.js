89f816af38d487c09b48c7cc2282128fb1327a45
irinag00 <irinagorlino@gmail.com>
2023-07-27 17:25:45 -0300

    agregue funciones

--- main.js
+++ main.js
@@ -10,68 +10,76 @@ let nombre;
 let apellido;
 let resultado;
 
-while (datosCorrectos == false){
-    nombre= prompt ("Ingrese su nombre");
-    apellido = prompt ("Ingrese su apellido");
-    validarDatos(nombre, apellido);
-}
+ingresarDatos();
+calcularMoneda(datosCorrectos);
 
-if (datosCorrectos == true){
-    
-    let entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos como menciona la tabla de abajo. Cuando desea cancelar, presione 'ESC'.");
-
-    while (entrada != "ESC") {
-      switch (entrada) {
-        case "BTC":
-            moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar BITCOIN.");
-            validarNro(moneda);
-            resultado= parseInt(moneda) / bitcoin;
-            alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} BTC.`)
-          break;
-    
-        case "ETH":
-            moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar ETHERIUM.");
-            validarNro(moneda);
-            resultado= parseInt(moneda) / etherium;
-            alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} ETH.`)
-          break;
-    
-        case "DAI":
-            moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar DAI.");
-            validarNro(moneda);
-            resultado= parseInt(moneda) / dai;
-            alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} DAI.`)
-          break;
-    
-        case "XRP":
-            moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar XRP.");
-            validarNro(moneda);
-            resultado= parseInt(moneda) / xrp;
-            alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} XRP.`)
-          break;
-    
-        case "BCH":
-            moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar BITCOIN CASH.");
-            validarNro(moneda);
-            resultado= parseInt(moneda) / bch;
-            alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} BCH.`)
-          break;
-        
-        case "USDT":
-            moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar TETHER.");
-            validarNro(moneda);
-            resultado= parseInt(moneda) / usdt;
-            alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} USDT`)
-            break;
-        
-        default:
-          alert("La moneda ingresada es incorrecta.");
-          break;
-      }
-      entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos como menciona la tabla de abajo. Cuando desea cancelar, presione 'ESC'.");
+function ingresarDatos(){
+    while (datosCorrectos == false){
+        nombre= prompt ("Ingrese su nombre");
+        apellido = prompt ("Ingrese su apellido");
+        validarDatos(nombre, apellido);
+        // return (nombre, apellido)
     }
 }
-datosCorrectos = false;
+
+function calcularMoneda(datosCorrectos){
+    if (datosCorrectos == true){
+    
+        let entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos como menciona la tabla de abajo. Cuando desea cancelar, presione 'ESC'.");
+    
+        while (entrada != "ESC") {
+          switch (entrada) {
+            case "BTC":
+                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar BITCOIN.");
+                validarNro(moneda);
+                resultado= parseInt(moneda) / bitcoin;
+                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} BTC.`)
+              break;
+        
+            case "ETH":
+                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar ETHERIUM.");
+                validarNro(moneda);
+                resultado= parseInt(moneda) / etherium;
+                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} ETH.`)
+              break;
+        
+            case "DAI":
+                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar DAI.");
+                validarNro(moneda);
+                resultado= parseInt(moneda) / dai;
+                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} DAI.`)
+              break;
+        
+            case "XRP":
+                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar XRP.");
+                validarNro(moneda);
+                resultado= parseInt(moneda) / xrp;
+                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} XRP.`)
+              break;
+        
+            case "BCH":
+                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar BITCOIN CASH.");
+                validarNro(moneda);
+                resultado= parseInt(moneda) / bch;
+                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} BCH.`)
+              break;
+            
+            case "USDT":
+                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar TETHER.");
+                validarNro(moneda);
+                resultado= parseInt(moneda) / usdt;
+                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} USDT`)
+                break;
+            
+            default:
+              alert("La moneda ingresada es incorrecta.");
+              break;
+          }
+          entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos como menciona la tabla de abajo. Cuando desea cancelar, presione 'ESC'.");
+        }
+    }
+    datosCorrectos = false;
+}
 
 function validarDatos(nombre, apellido){
     if (nombre != "" && apellido != ""){
