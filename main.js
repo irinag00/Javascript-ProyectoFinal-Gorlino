133c26d26b88f8442c6a87d235e1705df7731459
irinag00 <irinagorlino@gmail.com>
2023-08-16 17:10:10 -0300

    agregue filtros, busqueda y arrays de objetos

--- main.js
+++ main.js
@@ -10,76 +10,102 @@ let nombre;
 let apellido;
 let resultado;
 
+alert("¡Bienvenido a la Billetera de Cryptos!")
 ingresarDatos();
 calcularMoneda(datosCorrectos);
 
 function ingresarDatos(){
     while (datosCorrectos == false){
-        nombre= prompt ("Ingrese su nombre");
-        apellido = prompt ("Ingrese su apellido");
-        validarDatos(nombre, apellido);
-        // return (nombre, apellido)
+      nombre= prompt ("Ingrese su nombre");
+      apellido = prompt ("Ingrese su apellido");
+      validarDatos(nombre, apellido);
     }
+}
+
+function convertirMoneda (moneda, cantidad){
+  const tasas = {
+    BTC: bitcoin,
+    ETH: etherium,
+    DAI: dai,
+    XRP: xrp,
+    BCH: bch,
+    USDT: usdt
+  };
+  return cantidad / tasas[moneda];
 }
 
 function calcularMoneda(datosCorrectos){
     if (datosCorrectos == true){
+      
+      let transacciones = [];
     
-        let entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos (BTC, ETH, DAI, XRP, BCH, USDT). Cuando desea cancelar, presione 'ESC'.");
-    
-        while (entrada != "ESC") {
-          switch (entrada) {
-            case "BTC":
-                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar BITCOIN.");
-                validarNro(moneda);
-                resultado= parseInt(moneda) / bitcoin;
-                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} BTC.`)
-              break;
-        
-            case "ETH":
-                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar ETHERIUM.");
-                validarNro(moneda);
-                resultado= parseInt(moneda) / etherium;
-                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} ETH.`)
-              break;
-        
-            case "DAI":
-                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar DAI.");
-                validarNro(moneda);
-                resultado= parseInt(moneda) / dai;
-                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} DAI.`)
-              break;
-        
-            case "XRP":
-                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar XRP.");
-                validarNro(moneda);
-                resultado= parseInt(moneda) / xrp;
-                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} XRP.`)
-              break;
-        
-            case "BCH":
-                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar BITCOIN CASH.");
-                validarNro(moneda);
-                resultado= parseInt(moneda) / bch;
-                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} BCH.`)
-              break;
-            
-            case "USDT":
-                moneda = prompt("Ingrese la cantidad de pesos argentinos ($) que desea usar para comprar TETHER.");
-                validarNro(moneda);
-                resultado= parseInt(moneda) / usdt;
-                alert (`Usted señor/a: ${apellido}, ${nombre} ha ingresado una cantidad de: $${moneda} y ha adquirido ${resultado} USDT`)
-                break;
-            
+      let entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos (BTC, ETH, DAI, XRP, BCH, USDT). Cuando desea cancelar, escriba 'ESC'.");
+
+      while (entrada != "ESC"){
+        switch (entrada){
+          case "BTC":
+          case "ETH":
+          case "DAI":
+          case "XRP":
+          case "BCH":
+          case "USDT":
+            moneda = prompt("Ingrese la cantidad de pesos argentinos ($) para comprar la moneda ingresada.");
+            validarNro(moneda);
+            resultado = convertirMoneda(entrada, parseInt(moneda));
+
+            const transaccion = {
+              moneda: entrada,
+              cantidadPesos: moneda,
+              cantidadCripto: resultado
+            };
+
+            transacciones.push(transaccion);
+            break;
+
             default:
               alert("La moneda ingresada es incorrecta.");
               break;
-          }
-          entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos como menciona la tabla de abajo. Cuando desea cancelar, presione 'ESC'.");
         }
+        entrada = prompt("Ingresar la moneda a comprar (en mayúscula), indicando los símbolos (BTC, ETH, DAI, XRP, BCH, USDT). Cuando desea cancelar, escriba 'ESC'.");
+      }
+      for (const transaccion of transacciones) {
+        alert(`Usted señor/a: ${apellido}, ${nombre} ha ingresado:
+        Cantidad de Pesos ingresados: $${transaccion.cantidadPesos}
+        Cantidad de Cripto comprado: ${transaccion.cantidadCripto} ${transaccion.moneda}.
+        `);
+      }
+
+      busquedaMoneda(transacciones);
+      filtrarCrypto (transacciones);
+
+      function busquedaMoneda (transacciones){
+        let nombreMoneda= prompt("Ingrese el nombre de la moneda que desea buscar en sus compras");
+        let compra= transacciones.find((item) => item.moneda === nombreMoneda); 
+        if(compra){
+          alert(`
+            Moneda: ${compra.moneda}
+            Cantidad de Pesos ingresados: $${compra.cantidadPesos}
+            Cantidad de Cripto comprado: ${compra.cantidadCripto}
+          `);
+        } else {
+          alert("No has adquirido la crypto ingresada.");
+        };
+      }
+      function filtrarCrypto (transacciones){
+        let crypto= parseInt(prompt("Ingrese el número mínimo de cryptos compradas para filtrarlos."));
+        let filtrados= transacciones.filter((item) => item.cantidadCripto > crypto);
+
+        filtrados.forEach((item) => {
+          alert(`
+            Moneda: ${item.moneda}
+            Cantidad de Pesos ingresados: $${item.cantidadPesos}
+            Cantidad de Cripto comprado: ${item.cantidadCripto}
+          `);
+        })
+      }
     }
     datosCorrectos = false;
-}
+  }
 
 function validarDatos(nombre, apellido){
     if (nombre != "" && apellido != ""){
